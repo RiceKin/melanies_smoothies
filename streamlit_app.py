2797e28152acb63dbe2eb611d167d6eebfd6b0f9
RiceKin <106539143+RiceKin@users.noreply.github.com>
2024-05-22 11:26:26 +1000

    Update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
-st.write(
-    """Ordered that need to filled:"""
-)
+st.write("""Ordered that need to filled:""")
 
 # Get the current credentials
-session = get_active_session()
+#session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
